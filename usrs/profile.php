5f57bd851d558ae936ddc6dc043d76db3fe25abc
gloominati <sustix123@gmail.com>
2020-01-29 11:41:05 -0500

    Working on profiles

--- usrs/profile.php
+++ usrs/profile.php
@@ -1,5 +1,13 @@
 <?php
 
+    if(isset($_GET["user"])) {
+        $toshowu = $_GET["user"];
+    } else {
+        header('Location: /');
+    }
+
+    echo $toshowu;
+
     include "../inc/includables/header.php";
     include "../inc/includables/pages/network/header.php";
 
