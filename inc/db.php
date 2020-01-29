93138ab2f099cbd05a8211ece4c88324b35b4ca7
BeepBoop486 <53584109+BeepBoop486@users.noreply.github.com>
2020-01-29 10:30:12 -0500

    Delete db.php

--- inc/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	session_start();
-	$conn = mysqli_connect("localhost", "root", "gtaindrive", "clean");
-	if(!$conn) {
-		echo "There's been an error trying to connect to the Database";
-	}
-
-?>
