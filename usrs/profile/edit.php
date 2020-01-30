0e0fec73901c0b797c403f03d5f6a405cbf0bb74
gloominati <sustix123@gmail.com>
2020-01-30 15:28:29 -0500

    This could be made in this way

--- usrs/profile/edit.php
+++ usrs/profile/edit.php
@@ -8,5 +8,26 @@
 
     include "../../inc/includables/pages/network/header.php";
     include "../../inc/includables/header.php";
+    
+    $toshowu = $_SESSION["name"];
+
+    $stmt1 = $conn->prepare("SELECT name,bio,regdate,fname,lname,country,bday,occupation,mail,phonen FROM users WHERE name=?");
+    $stmt1->bind_param("s", $toshowu);
+    $stmt1->execute();
+    $stmt1->bind_result($uname, $bio, $regdate, $fname, $lname, $country, $bday, $occupation, $mail, $phonen);
+    $stmt1->fetch();
+    $stmt1->close();
+
+    $inabout = true;
 
 ?>
+
+<div class="container" style="margin-top: 50px;">
+    <div class="row">
+        <div class="col-md-10 no-paddin-xs">
+
+            <?php include "prof_left.php"; ?>
+
+        </div>
+    </div>
+</div>
