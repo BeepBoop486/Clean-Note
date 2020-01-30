7214c6423b93ac9f9555c52801957fe91b8a91e0
gloominati <sustix123@gmail.com>
2020-01-30 13:18:15 -0500

    Added side navigation to the about page

--- usrs/profile/prof_left.php
+++ usrs/profile/prof_left.php
@@ -33,12 +33,22 @@
         <div class="panel">
 
             <div class="user-heading round">
-                <a href="#">
+                <a href="/p/<?php echo $uname; ?>">
                     <img src="/img/pp.jpg" alt="">
                 </a>
                 <h1><?php echo $uname; ?></h1>
                 <p><?php echo $bio; ?></p>
             </div>
+
+            <ul class="nav nav-pills nav-stacked">
+                <li><a href="/p/<?php echo $uname; ?>"><i class="fa fa-user"></i>Profile</a></li></li>
+                <li><a href="/a/<?php echo $uname; ?>"><i class="fa fa-info-circle"></i> About</a></li>
+                <?php if(isset($_SESSION["name"]) && $_SESSION["name"] == $uname) : ?>
+                <li>
+                    <a href="#"><i class="fa fa-edit"></i> Edit profile</a>
+                </li>
+                <?php endif; ?>
+            </ul>
         
         </div>
 
