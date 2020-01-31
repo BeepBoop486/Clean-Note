242c4a4aeca30ae4518211715de06580c5c9e95c
gloominati <sustix123@gmail.com>
2020-01-31 15:36:50 -0500

    bug fixing

--- inc/db.template.php
+++ inc/db.template.php
@@ -1,6 +1,6 @@
 <?php
 
-	include "../globals.php";
+	include $_SERVER["DOCUMENT_ROOT"] . "/globals.php";
 
 	session_start();
 	$conn = mysqli_connect($globals["DB_HOST"], $globals["DB_USER"], $globals["DB_PASS"] , $globals["DB_NAME"]);
