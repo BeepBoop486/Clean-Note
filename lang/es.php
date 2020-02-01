ec387e4bf510f382283266e150c407dabe01e471
MyaUwU <53584109+BeepBoop486@users.noreply.github.com>
2020-01-31 19:24:35 -0500

    Now you can switch between Spanish and English

---
+++ lang/es.php
@@ -0,0 +1,34 @@
+<?php 
+
+	$lang["What's in your mind today?"] = "¿En qué estás pensando?";
+	$lang["Posted:"] = "Publicó:";
+	$lang["Comment"] = "Comentar";
+	$lang["Add a comment"] = "Agrega un comentario";
+	$lang["Click here for full view"] = "Haz clic para una vista completa";
+	$lang["There's been an error trying to fetch the posts"] = "Ha habido un error obteniendo los posts";
+	$lang["Home"] = "Inicio";
+	$lang["Logout"] = "Cerrar sesión";
+	$lang["There's been an error trying to post"] = "Ha habido un error al publicar tu post";
+	$lang["Username..."] = "Nombre de usuario...";
+	$lang["Password..."] = "Contraseña...";
+	$lang["Log in"] = "Iniciar sesión";
+	$lang["Signup"] = "Registrarse";
+	$lang["Edit info"] = "Editar la información";
+	$lang["UserName"] = "UserName";
+	$lang["Bio"] = "Bio";
+	$lang["Registration Date"] = "Fecha de registro";
+	$lang["Personal info"] = "Información personal";
+	$lang["First name"] = "Nombre(s)";
+	$lang["Last name"] = "Apellido(s)";
+	$lang["Country"] = "País";
+	$lang["Birthday"] = "Fecha de cumpleaños";
+	$lang["Occupation"] = "Trabajo";
+	$lang["Mail"] = "Correo";
+	$lang["Edit"] = "Editar";
+	$lang["Phonen"] = "Número telefónico";
+	$lang["Profile"] = "Perfil";
+	$lang["About"] = "Acerca de";
+	$lang["Liked"] = "Me gusta";
+	$lang["Edit profile"] = "Editar perfil";
+
+?>
